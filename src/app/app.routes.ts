a2f7c9afc0e0b3b50b2e6c3fed0f4bd005b13cea
Magda Paicu <paicumagda962@gmail.com>
2024-12-17 15:54:46 +0200

    crearea navbar-ului

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        redirectTo:'register',
+        pathMatch: 'full'
+    }, 
+    {
+        path:'',
+        component:RegisterComponent
+    },
+    {
+        path:'home-page',
+        component:HomePageComponent
+    }
+];
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [MatFormFieldModule, MatInputModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
+  constructor(private router: Router) {}
 
+  goToHomePage() {
+    this.router.navigate(['/home-page']);
+  }
 }
